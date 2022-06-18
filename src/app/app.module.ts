693aa67726d037dffcf740d26b116c4878b225c1
Francis Kamau <fkmuiruri8@gmail.com>
2022-06-18 20:09:08 +0300

    create crud app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
+import { EditComponent } from './components/edit/edit.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    IndexComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'create',component:CreateComponent},
+      {path:'edit/:id',component:EditComponent},
+      {path:'index',component:IndexComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
